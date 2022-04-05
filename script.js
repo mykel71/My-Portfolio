76733726cd02c95c2d7f9c139dc626fd8cbc6659
Shepherd <shepherd.munemo@outlook.com>
2022-04-05 21:32:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,13 +20,13 @@ $(document).ready(function() {
 
     //typing animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Developer", "Web Developer", "Microsoft Certified: Azure Database Administrator Associate", "Microsoft Certified: Azure Developer Associate", "Freelancer"],
+        strings: [ "C# / .Net", "Software Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Software Developer", "Web Developer", "Microsoft Certified: Azure Database Administrator Associate", "Microsoft Certified: Azure Developer Associate", "Freelancer"],
+        strings: ["C# / .Net","Software Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
